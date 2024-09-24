9c8201f3d3bef964d9f9c5da4e0226562c52db4e
kasundraJeet <kasundrajeet29@gmail.com>
2024-09-25 01:45:55 +0530

    update data

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()]
+  site: '',
+  integrations: [tailwind(), sitemap()]
 });
