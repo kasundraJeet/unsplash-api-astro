cf9d6d24852f87f87dfef8cd09fa25df7a859423
kasundraJeet <kasundrajeet29@gmail.com>
2024-09-25 01:48:48 +0530

    added new packnage

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 export default defineConfig({
   output: 'server',
-  site: '',
-  integrations: [tailwind(), sitemap()]
+
+  // site: '',
+  integrations: [tailwind(), sitemap()],
+
+  adapter: vercel()
 });
