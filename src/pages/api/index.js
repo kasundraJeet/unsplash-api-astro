f9753df1a5ace82e3088ce97e0f4a27008e56a35
kasundraJeet <kasundrajeet29@gmail.com>
2024-09-26 23:35:30 +0530

    update ui

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const accessKey = import.meta.env.ACCESS_KEY;
 
-export async function fetchUnsplashPhotos(query) {
+export async function fetchUnsplashPhotos(query , page) {
   let url;
   if (query) {
-    url = `https://api.unsplash.com/search/photos?page=1&query=${query}`;
+    url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}`;
   } else {
-    url = `https://api.unsplash.com/photos?page=4`;
+    url = `https://api.unsplash.com/photos?page=${page}`;
   }
 
   try {
